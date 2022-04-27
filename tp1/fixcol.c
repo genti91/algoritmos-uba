d64062d1a8931e0ab464cef71f7efa28b3e0dcc3
genti91 <franco.g7@hotmail.com>
2022-04-27 00:47:23 -0300

    tp1

--- tp1/fixcol.c
+++ tp1/fixcol.c
@@ -1,12 +1,10 @@
-#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
 bool chequear_parametros(int argc, char const *argv){
-    if (argc < 2 || argc > 3 || !atoi(argv))
-    {
+    if (argc < 2 || argc > 3 || !atoi(argv)){
         fputs("Error: Cantidad erronea de parametros\n", stderr);
         return false;
     }
@@ -23,45 +21,26 @@ FILE* abrir_archivo(char const *nombre_de_archivo){
 }
 
 void separar_texto(FILE* texto, int max_chars){
-    char* linea = NULL;
-    size_t tam;
+    char renglon[max_chars];
 
-    while ((getline(&linea, &tam, texto)) != EOF)
-    {   
-        int contador = 1;
-        for (size_t i = 0; i < strlen(linea); i++)
-        {   
-            if (linea[i] != '\n')
-            {
-                fputc(linea[i], stdout);
-            }
+    while ((fgets(renglon, max_chars+1, texto)) != NULL){
 
-            if (contador == max_chars)
-            {
+        if (renglon[0] != '\n'){
+            fputs(renglon, stdout);
+
+            if (renglon[strlen(renglon)-1] != '\n'){
                 fputc('\n', stdout);
-                contador = 0;
             }
-            
-            contador++;
-        }
-
-        if (strlen(linea)-2 != max_chars)
-        {
-            fputc('\n', stdout);
-                
         }
     }
-    free(linea);
 }
 
-int main(int argc, char const *argv[])
-{   
+int main(int argc, char const *argv[]){
     if (!chequear_parametros(argc, argv[1])){
         return -1;
     }
-
+    
     int max_chars = atoi(argv[1]);
-
     FILE* texto = stdin;
 
     if (argc == 3){
